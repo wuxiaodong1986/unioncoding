cd98c04f9fc54f966cf0808d8466cdb593c6cca9
wuxiaodong <wuxiaodong1@weshare.com.cn>
2017-04-06 23:01:07 +0800

    first version

---
+++ src/main/java/com/union/UnioncodingApplication.java
@@ -0,0 +1,12 @@
+package com.union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnioncodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnioncodingApplication.class, args);
+	}
+}
