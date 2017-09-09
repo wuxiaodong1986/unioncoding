15ac4f58832ad50b010708657a9e89421ecbdb79
wuxiaodong <308200039@qq.com>
2017-09-09 23:45:20 +0800

    user first version

---
+++ src/main/java/com/unioncoding/config/PageHistoryFilter.java
@@ -0,0 +1,59 @@
+package com.unioncoding.config;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 页面访问历史过滤器
+ * Created by 吴晓冬 on 2017/9/9.
+ */
+public class PageHistoryFilter implements Filter
+{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpSession session = request.getSession();
+
+        List<String> pageHistory = (List<String>)session.getAttribute("pageHistory");
+
+        if (null == pageHistory)
+        {
+            pageHistory = new ArrayList<>();
+        }
+
+        String url = request.getServletPath();
+
+        if (!url.contains(".") && "GET".equals(request.getMethod()))
+        {
+            pageHistory.add(0, url);
+        }
+
+        session.setAttribute("pageHistory", pageHistory);
+
+        try
+        {
+            chain.doFilter(servletRequest, servletResponse);
+        }
+        catch (Throwable e)
+        {
+            pageHistory.remove(0);
+        }
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
+}
---
+++ src/main/java/com/unioncoding/config/UnionFilterConfig.java
@@ -0,0 +1,31 @@
+package com.unioncoding.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 吴晓冬 on 2017/9/9.
+ */
+@Configuration
+public class UnionFilterConfig
+{
+    @Bean
+    public FilterRegistrationBean corsFilter()
+    {
+        PageHistoryFilter pageHistoryFilter = new PageHistoryFilter();
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setName("pageHistoryFilter");
+        registrationBean.setFilter(pageHistoryFilter);
+        registrationBean.setOrder(1);
+
+        List<String> urlList = new ArrayList<>();
+        urlList.add("/*");
+        registrationBean.setUrlPatterns(urlList);
+
+        return registrationBean;
+    }
+}
